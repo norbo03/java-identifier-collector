c5364463a5bedb6f984fd56f4c930d551d2ef864
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-05-17 20:50:03 +0200

    Remove type params

--- find_java_names.sh
+++ find_java_names.sh
@@ -13,10 +13,16 @@ input_file=$1
 regex='^public +(abstract +)?(class|interface|enum) +([a-zA-Z_$][a-zA-Z0-9_$]*)'
 
 # Extract desired lines and filter out unwanted lines
-names=$(grep -E "$regex" "$1" | sed -E 's/^public +(abstract +)?(class|interface|enum) +//' | sed -E 's/ +extends.*//; s/ +implements.*//; s/ +\{.*//' | grep -v '\.$')
+names=$(grep -E "$regex" "$input_file" | sed -E 's/^public +(abstract +)?(class|interface|enum) +//' | sed -E 's/ +extends.*//; s/ +implements.*//; s/ +\{.*//' | grep -v '\.$')
+
+# Extract last name after .
+#names=$(echo $names | grep -o '[^.]*$')
+#names=$(echo $names | rev | cut -d"." -f1 | rev)
+#names=$(echo $names | sed -E "s/.*\. \?//g")
+names=$(echo "$names" | awk -F '.' '{print $NF}' | awk '{gsub(/<.*/, ""); print}')
 
 # Remove type parameters from the names
-#names=$(echo "$names" | sed -E 's/<.*>//g')
+#names=$(echo "$names" | sed -E "s/<[^>]*//g")
 
 # Remove package hierarchies and keep only identifiers
 #names=$(echo "$names" | sed -E 's/^.*\.([^.]+\$)?([^.]+\$)?([^.]+\$)?([^.]+\$)?([^.]+)$/\1\n\2\n\3\n\4\n\5/')
