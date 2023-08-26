1d3c79b5bbe721f843470811abcd6a228ad3fa32
Nagy Norbert-Botond <nagynorbi0303@gmail.com>
2023-08-26 09:25:11 +0200

    Update find_identifiers.sh

--- find_identifiers.sh
+++ find_identifiers.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Usage: ./find_identifiers.sh <source_folder>
 
 # Extract all jmod files then all the classes will be available for later usage
 echo "Extracting jmod files.."
-source_folder="/home/norbi/Desktop/ELTE/SpotBugs/research/identifiers"
+source_folder="$1"
 
 find /usr/lib/jvm/jdk-20 -name *.jmod -exec jmod extract --dir $source_folder/sources {} \;
 echo "Extracted all jmod files into $source_folder/sources!"
@@ -11,5 +12,3 @@ echo "Extracted all jmod files into $source_folder/sources!"
 echo "Collecting  all .class files.."
 find $source_folder/sources -name *.class -exec javap -constants -public -classpath $source_folder/sources {} \; > ./identifiers.txt
 echo "All decompiled .class collected into $source_folder/identifiers.txt"
-
-#"./extract_identifiers.sh" "$source_folder/identifiers.txt"
